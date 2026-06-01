3032e0662325731bb20bf2104f2a8f857773fc96
qiuji <2731226814@qq.com>
2026-06-01 21:24:14 +0800

    Initial commit: Spring Boot project with JDK 21

---
+++ src/main/java/com/qiuji/qaiagent/QAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.qiuji.qaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  QAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QAiAgentApplication.class, args);
+    }
+
+}
