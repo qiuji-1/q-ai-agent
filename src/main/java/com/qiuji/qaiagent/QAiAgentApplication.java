e8e29951dbe88707c2f29ffc3308c7ec054fbf20
qiuji <2731226814@qq.com>
2026-06-04 21:58:10 +0800

    修改启动类，新增识别方法，添加一个注释可以选择防止同时运行两个AI模型导致的重复问题

--- src/main/java/com/qiuji/qaiagent/QAiAgentApplication.java
+++ src/main/java/com/qiuji/qaiagent/QAiAgentApplication.java
@@ -1,12 +1,14 @@
 package com.qiuji.qaiagent;
 
+import com.alibaba.cloud.ai.autoconfigure.dashscope.DashScopeChatAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {DashScopeChatAutoConfiguration.class}) //防止同时运行两个AI模型导致的重复问题
 public class  QAiAgentApplication {
 
     public static void main(String[] args) {
+        System.out.println("启动项目的main方法被调用了!");
         SpringApplication.run(QAiAgentApplication.class, args);
     }
 
