266426918c62474a2867b2902faf2ba592768956
qiuji <2731226814@qq.com>
2026-06-12 19:51:54 +0800

    通过重新运行chatAPP测试类， 检查AI模型是否更换成功

--- src/test/java/com/qiuji/qaiagent/app/ChatAppTest.java
+++ src/test/java/com/qiuji/qaiagent/app/ChatAppTest.java
@@ -19,17 +19,17 @@ class ChatAppTest {
     void testChat() {
         String chatId = UUID.randomUUID().toString();
         // 第一轮
-        String message = "你好，我是秋霁";
+        String message = "你的数据截至到几年几月";
         String answer = chatApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
         // 第二轮
-        message = "我想要更好地跟我的朋友凯伦聊天";
-        answer = chatApp.doChat(message, chatId);
-        Assertions.assertNotNull(answer);
+       // message = "我想要更好地跟我的朋友凯伦聊天";
+       // answer = chatApp.doChat(message, chatId);
+       // Assertions.assertNotNull(answer);
         // 第三轮
-        message = "我的朋友叫什么？刚跟你说过，帮我回忆一下";
-        answer = chatApp.doChat(message, chatId);
-        Assertions.assertNotNull(answer);
+       // message = "我的朋友叫什么？刚跟你说过，帮我回忆一下";
+        //answer = chatApp.doChat(message, chatId);
+       // Assertions.assertNotNull(answer);
     }
     @Test
     void doChatWithReport() {
@@ -48,13 +48,13 @@ class ChatAppTest {
     @Test
     void doChatWithTools(){
         // 测试联网搜索问题的答案
-        testMessage("请帮我搜索一下'Spring AI 1.1.7的新特性？");
+        testMessage("请帮我搜索一下apple的来历");
         // 测试资源下载：图片下载
-        testMessage("请帮我下载 Google 的 Logo 图片，URL是 https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png ，文件名保存为 google-logo.png");
+       // testMessage("请帮我下载 Google 的 Logo 图片，URL是 https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png ，文件名保存为 google-logo.png");
         // 测试文件操作：保存用户档案
-        testMessage("请将以下内容保存到文件 user-profile.txt：姓名：秋霁；职业：AI工程师；爱好：编程");
+       // testMessage("请将以下内容保存到文件 user-profile.txt：姓名：秋霁；职业：AI工程师；爱好：编程");
         // 测试 PDF 生成
-        testMessage("请生成一份名为 'ai-report.pdf' 的PDF报告，内容为：人工智能发展趋势报告 - 2026年版");
+        //testMessage("请生成一份名为 'apple.pdf' 的PDF报告，内容为：apple test - 2026年6月12日版");
     }
     private void testMessage(String message) {
         String chatId = UUID.randomUUID().toString();
@@ -84,11 +84,11 @@ class ChatAppTest {
        //     chatId
        // );
 
-        //测试4 图片搜索MCP
-        //testMcpMessage(
-        //        "请帮我搜索一些咖啡馆图片",
-        //        chatId
-        //);
+       // 测试4 图片搜索MCP
+        testMcpMessage(
+                "请帮我搜索一些咖啡馆图片",
+                chatId
+        );
     }
     private void testMcpMessage(String message, String chatId) {
         System.out.println("用户请求: " + message);
